5f14e30b6a06097fdf7517ae5ecb3aa3b9aeef44
Julio Cesar <julczar21@outlook.com>
2020-06-02 20:57:56 -0300

    concluída integração do sensor de temperatura

--- index.c
+++ index.c
@@ -12,8 +12,7 @@ Servo servo_3;
 Servo servo_4;
 Servo servo_5;
 
-void setup()
-{
+void setup() {
   //Leituras analógicas
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
@@ -33,30 +32,29 @@ void setup()
   servo_5.attach(5);
 }
 
-void loop()
-{
+void loop() {
+  //Leituras dos sensores de temperatura
+  SensorTemp_1 = (-40 + 0.488155 * (analogRead(A2) - 20) + 1);
+  SensorTemp_2 = (-40 + 0.488155 * (analogRead(A1) - 20) + 1);
+  SensorTemp_3 = (-40 + 0.488155 * (analogRead(A0) - 20) + 1);
+
   //Leitura dos sensores de humidade
   SensorUmid_1 = analogRead(A3);
   SensorUmid_2 = analogRead(A4);
   SensorUmid_3 = analogRead(A5);
 
   //Transforma a leitura do sensor de umidade em porcentagem
-  SensorUmid_1 = map(SensorUmid_1, 1023, 0, 0, 100)
-  SensorUmid_2 = map(SensorUmid_2, 1023, 0, 0, 100)
-  SensorUmid_3 = map(SensorUmid_3, 1023, 0, 0, 100)
-
-  //Leituras dos sensores de temperatura
-  SensorTemp_1 = (-40 + 0.488155 * (analogRead(A2) - 20) + 1);
-  SensorTemp_2 = (-40 + 0.488155 * (analogRead(A1) - 20) + 1);
-  SensorTemp_3 = (-40 + 0.488155 * (analogRead(A0) - 20) + 1);
+  SensorUmid_1 = map(SensorUmid_1, 1023, 0, 0, 100);
+  SensorUmid_2 = map(SensorUmid_2, 1023, 0, 0, 100);
+  SensorUmid_3 = map(SensorUmid_3, 1023, 0, 0, 100);
 
   //Modulo do primeiro sensor de umidade
   if (SensorUmid_1 <= 50) {
-    servo_3.write(240);
+    servo_3.write(225 + SensorTemp_1);
   } else if (SensorUmid_1 <= 60) {
-    servo_3.write(150);
+    servo_3.write(145 + SensorTemp_1);
   } else if (SensorUmid_1 <= 74) {
-    servo_3.write(90);
+    servo_3.write(90 + SensorTemp_1);
     digitalWrite(6, HIGH);
   } else {
     servo_3.write(0);
@@ -64,12 +62,12 @@ void loop()
   }
 
   //Segundo sensor de umidade
-  if (SensorUmid_2 <= 50) {
-    servo_4.write(240);
-  } else if (SensorUmid_2 <= 60) {
-    servo_4.write(150);
-  } else if (SensorUmid_2 <= 74) {
-    servo_4.write(90);
+  if (SensorUmid_2 <= 30) {
+    servo_4.write(225 + SensorTemp_2);
+  } else if (SensorUmid_2 <= 50) {
+    servo_4.write(145 + SensorTemp_2);
+  } else if (SensorUmid_2 <= 80) {
+    servo_4.write(90 + SensorTemp_2);
     digitalWrite(7, HIGH);
   } else {
     servo_4.write(0);
@@ -78,11 +76,11 @@ void loop()
 
   //terceiro sensor de umidade
   if (SensorUmid_3 <= 50) {
-    servo_5.write(240);
-  } else {if (SensorUmid_3 <= 60) {
-    servo_5.write(150);
+    servo_5.write(225 + SensorTemp_2);
+  } else if (SensorUmid_3 <= 60) {
+    servo_5.write(145 + SensorTemp_2);
   } else if (SensorUmid_3 <= 74) {
-    servo_5.write(90);
+    servo_5.write(90 + SensorTemp_2);
     digitalWrite(8, HIGH);
   } else {
     servo_5.write(0);
